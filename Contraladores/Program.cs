ca0b2b1ae7ae734c53a0afaa85320fadb3ddb2ac
Eduardo Ballesteros Pérez <ballesterospereze@gmail.com>
2023-10-23 23:25:00 +0200

    Agregar archivos de proyecto.

---
+++ Contraladores/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using CuadradoEntero.Servicios;
+
+namespace CuadradoEntero.Contraladores
+{
+    /// <summary>
+    /// Clase principal 
+    /// 241023 - ebp
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método principal del programa
+        /// 231023
+        /// </summary>
+        /// <param name="args"></param>
+        /// <author>ebp</author>
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuadradoEntero.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public int MostrarYRecogerSeleccion()
+        {
+            int opcionSeleccion;
+
+            Console.WriteLine("Introduzca un número para conocer si el número se puede expresar como el cuadrado de otro número entero");
+            opcionSeleccion = Convert.ToInt32(Console.ReadLine());
+
+            return opcionSeleccion;
+        }
+    }
+}
